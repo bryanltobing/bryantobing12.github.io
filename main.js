084499415d78656b1d97bbd739ed3b290379587b
bryantobing12 <bryantobing0@gmail.com>
2020-09-19 13:08:09 +0700

    new look for my personal website

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-function showBiodata()
-{
-    document.getElementById("Clicked").innerHTML = 
-    "<button onclick='hideBiodata()'>Hide Biodata</button>"+
-    "<div class='mantap1'><br>"+
-    "<b>Nama</b>   <pre>Bryan Lumbantobing</pre><br>"+
-    "<b>Email</b>  <pre>bryantobing0@gmail.com</pre><br>"+
-    "<b>Prodi</b>  <pre>D3 Teknologi Informasi</pre><br>"+
-    "<b>Job</b>  <pre>FullStack Web Developer</pre>"+
-    "</div>";
+if(localStorage.getItem('theme') === 'dark') {
+    document.body.id = "dark";
+} else {
+    document.body.removeAttribute('id');
 }
 
-function hideBiodata()
-{
-    document.getElementById("Clicked").innerHTML = 
-    "<button onclick='showBiodata()'>Show Biodata</button>";
+const setDarkMode = (boolean) => {
+    if(boolean) {
+        document.body.id = "dark";
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+        document.body.removeAttribute('id');
+    }
 }
