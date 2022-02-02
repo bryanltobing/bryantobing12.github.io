a4bb40067f1866a6e19e0dd1504a03ac079d2e49
bryantobing12 <bryantobing0@gmail.com>
2022-02-02 20:09:05 +0700

    Add redirection to the main domain

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-if(localStorage.getItem('theme') === 'dark') {
-    document.body.id = "dark";
+window.onload = () => {
+  window.location.href = "https://bryantobing.com";
+};
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.id = "dark";
 } else {
-    document.body.removeAttribute('id');
+  document.body.removeAttribute("id");
 }
 
 const setDarkMode = (boolean) => {
-    if(boolean) {
-        document.body.id = "dark";
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-        document.body.removeAttribute('id');
-    }
-}
+  if (boolean) {
+    document.body.id = "dark";
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    document.body.removeAttribute("id");
+  }
+};
